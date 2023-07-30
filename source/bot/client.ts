6d20bb3f5965fe781ad97f11bb8c8624c0226d57
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-07-30 13:29:26 +1000

    -removed extra log

--- source/bot/client.ts
+++ source/bot/client.ts
@@ -36,11 +36,7 @@ process.on('SIGHUP', () => {
 
 
 export function fetchWrapper<T>(p: Promise<T>): Promise<T | null> {
-	return new Promise((res, rej) => {
-		p.then(res)
-			.catch(e => {
-				console.error(e);
-				res(null);
-			})
+	return new Promise((res) => {
+		p.then(res).catch(_ => res(null))
 	});
 }
