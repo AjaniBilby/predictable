6eaa79f0f39d76a47b7cd9f4f958c1bbd0458399
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-07-31 17:31:23 +1000

    Update server.tsx

--- source/website/routes/server.tsx
+++ source/website/routes/server.tsx
@@ -1,13 +1,8 @@
-import { Link, RenderArgs } from "htmx-router";
+import { RenderArgs } from "htmx-router";
 import * as elements from 'typed-html';
 
 export async function Render(rn: string, {res, Outlet}: RenderArgs) {
 	res.setHeader('Cache-Control', "public, max-age=7200");
 
-	const inner = await Outlet();
-
-	return <div id={rn}>
-		<Link to="/server">Server List</Link>
-		{inner}
-	</div>;
+	return <div id={rn}>{await Outlet()}</div>;
 }
