0d4ba21b67b87b2a15f4f4052c66b5bff1417cb1
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-08-04 08:43:25 +1000

    ~Catch critical errors

--- source/website/server.ts
+++ source/website/server.ts
@@ -10,6 +10,7 @@ import * as path from 'node:path';
 
 import { Router } from "./router";
 import { Override, Redirect } from "htmx-router";
+import { bot } from "../logging";
 
 const staticDir = path.join(__dirname,
 	process.argv[0].includes("ts-node") ? '../../public' : "../public"
@@ -33,16 +34,22 @@ const app = http.createServer(async (req, res) => {
 		}
 	}
 
-	const out = await Router.render(req, res, url);
-	if (out instanceof Redirect) {
-		res.statusCode = 302;
-		res.setHeader('Location', out.location);
-		return res.end();
-	} else if (out instanceof Override) {
-		res.end(out.data);
-	} else {
-		res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-		res.end("<!DOCTYPE html>"+out);
+	try {
+		const out = await Router.render(req, res, url);
+		if (out instanceof Redirect) {
+			res.statusCode = 302;
+			res.setHeader('Location', out.location);
+			return res.end();
+		} else if (out instanceof Override) {
+			res.end(out.data);
+		} else {
+			res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+			res.end("<!DOCTYPE html>"+out);
+		}
+	} catch(e: any) {
+		bot("CRIT", e.stack || e.toString());
+		res.statusCode = 500;
+		res.end(e.stack || e.toString())
 	}
 	return;
 });
