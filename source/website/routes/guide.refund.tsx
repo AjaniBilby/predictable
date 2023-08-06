a83c5546479db0b4eeb96ac836a2861f30be4d4a
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-08-06 12:15:19 +1000

    +Updated guides

--- source/website/routes/guide.refund.tsx
+++ source/website/routes/guide.refund.tsx
@@ -3,16 +3,20 @@ import * as elements from 'typed-html';
 
 
 export async function Render(rn: string, {res, setTitle}: RenderArgs) {
-	setTitle("Managing Permissions - Predictable Bot");
+	setTitle("Refunding a Prediction - Predictable Bot");
 
 	return <div id={rn}>
-		<h2>Refund Predictions</h2>
+		<h2>Refund a Predictions</h2>
+		<h2>Refund</h2>
 		<p>
-			The "/prediction auto-refund" command will check for any on-going prediction who's message has been deleted.
-			All of these predictions will then be refunded automatically.
+			If you right click on a prediction message you'll see Apps &gt; Prediction Refund.
+			After clicking this option you will get a confirmation button.
+			Once that is clicked all wagers will be refunded, the prediction will be deleted from the database - and the message will be updated to have a status of refunded
 		</p>
+		<h2>Auto Refund</h2>
 		<p>
-			So if you want to refund an existing prediction, delete the message - then run "/prediction auto-refund"
+			The "/prediction auto-refund" command will check for any prediction who's message has been deleted but hasn't been paid out.
+			All of these predictions will then be refunded automatically.
 		</p>
 	</div>
 }
--- source/website/routes/guide.tsx
+++ source/website/routes/guide.tsx
@@ -13,7 +13,8 @@ export async function Render(rn: string, {Outlet, setTitle}: RenderArgs) {
 			<li><Link to="/guide/getting-started">Getting Started</Link></li>
 			<li><Link to="/guide/prediction">Running a Prediction</Link></li>
 			<li><Link to="/guide/permission">Managing Permissions</Link></li>
-			<li><Link to="/guide/refund">Refund Prediction</Link></li>
+			<li><Link to="/guide/refund">Refund a Prediction</Link></li>
+			<li><Link to="/guide/general">General Use</Link></li>
 		</ul>
 		{await Outlet()}
 	</div>;
