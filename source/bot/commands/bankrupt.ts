b4dba4c1d09d847ab358627eb66081e611371a14
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-08-09 13:40:00 +1000

    ~Made bankrupt refund all open wagers first

--- source/bot/commands/bankrupt.ts
+++ source/bot/commands/bankrupt.ts
@@ -7,7 +7,7 @@ export const name = "bankrupt";
 export function bind(subcommand: SlashCommandSubcommandBuilder) {
 	return subcommand
 		.setName(name)
-		.setDescription("Reset your account balance if you're in debt");
+		.setDescription("Refunds all active wagers, and resets your account balance if you're in debt");
 }
 
 export async function execute (scope: ChatInputCommandInteraction<CacheType>) {
@@ -33,22 +33,49 @@ export async function execute (scope: ChatInputCommandInteraction<CacheType>) {
 		await scope.editReply({ content: `You don't have an account yet in this guild\nStart betting for your opening balance` });
 		return;
 	}
-
 	if (account.balance > 1) {
 		await scope.editReply({ content: `You can only declare bankrupt if your balance is less than 1\nYou've got to work your way up` });
 		return;
 	}
 
-	const [updatedGuild, updatedAccount] = await prisma.$transaction([
-		prisma.guild.update({
-			where: { id: guildID },
-			data: { kitty: { decrement: 2 } }
+	const wagerSelection = {
+		userID: userID,
+		prediction: { status: "OPEN", guildID },
+	};
+	const [wagers, _] = await prisma.$transaction([
+		prisma.wager.findMany({
+			where: wagerSelection
 		}),
-		prisma.account.update({
-			where: { guildID_userID: { userID, guildID } },
-			data: { balance: 2 }
+		prisma.wager.deleteMany({
+			where: wagerSelection
 		})
 	]);
 
-	await scope.editReply({ content: `Your balance is ${updatedAccount.balance}` });
+
+	const refunded = wagers.reduce((s, x) => x.amount+s, 0);
+	const updated = await prisma.account.update({
+		where: { guildID_userID: { userID, guildID } },
+		data: {
+			balance: { increment: refunded }
+		}
+	});
+
+	await scope.editReply({ content: `Refunded \$${refunded} from existing wagers` });
+
+	if (updated.balance < 1) {
+		await prisma.$transaction([
+			prisma.guild.update({
+				where: { id: guildID },
+				data: { kitty: { decrement: 10 } }
+			}),
+			prisma.account.update({
+				where: { guildID_userID: { userID, guildID } },
+				data: { balance: { increment: 10 } }
+			})
+		]);
+
+		await scope.followUp({ content: "Took $10 from the server's kitty, so your balance is now 10" });
+	} else {
+		await scope.followUp({ content: `Your balance is ${updated.balance}` });
+	}
 }
