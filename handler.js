2551c6d7dc57765d146fbb1813a88dfb4a865e9d
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-08-29 09:35:26 +1000

    ~Auto restart bot on crash

--- handler.js
+++ handler.js
@@ -6,33 +6,38 @@ const platform = os.platform();
 const isUNIX = platform === 'darwin' || platform === 'linux' || platform === 'freebsd' || platform === 'openbsd' || platform === 'sunos' || platform === 'aix';
 const target = process.argv[2];
 
-const child = spawn('node', [target], {
-	stdio: [
-		'inherit', // stdin
-		'pipe',    // stdout
-		'pipe'     // stderr
-	]
-});
-
 const logStream = fs.createWriteStream('log-all.txt', { flags: 'a' });
-child.stdout.pipe(logStream);
-child.stderr.pipe(logStream);
+let child;
 
-// Forward signals to the child
-if (isUNIX) {
-	process.on('SIGUSR1', function () {
-		child.kill('SIGUSR1');
+function StartChild() {
+	child = spawn('node', [target], {
+		stdio: [
+			'inherit', // stdin
+			'pipe',    // stdout
+			'pipe'     // stderr
+		]
+	})
+
+	child.stdout.pipe(logStream);
+	child.stderr.pipe(logStream);
+
+	// Forward signals to the child
+	if (isUNIX) {
+		process.on('SIGUSR1', function () {
+			child.kill('SIGUSR1');
+		});
+	}
+
+	process.on('SIGTERM', function () {
+		child.kill('SIGTERM');
 	});
+
+	child.on('close', ChildCloseHandler);
 }
 
-process.on('SIGTERM', function () {
-	child.kill('SIGTERM');
-});
-
-// Handle the child process closing
-child.on('close', (code) => {
+function ChildCloseHandler(code) {
 	console.log(`Child process exited with code ${code}`);
-	logStream.write(`\nExited ${code}`);
-	logStream.end();
-	process.exit(code);
-});
+	logStream.write(`\nExited ${code}\n\n`);
+
+	StartChild();
+}
